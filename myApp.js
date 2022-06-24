16b6b3956c66a35dcdf30e84bee0dc21bffc25a4
WiseMrMusa <abdulkareemusa01@gmail.com>
2022-06-24 19:32:39 +0100

    This is a fork

--- myApp.js
+++ myApp.js
@@ -1,3 +1,8 @@
+require('mongodb');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 require('dotenv').config();
 
 
